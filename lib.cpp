3e6d0a3a7f6f1face310b6a4b4cbb54139c4fd12
AuroraPortesine <100139517+AuroraPortesine@users.noreply.github.com>
2022-05-06 16:44:49 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include "lib.h"
 
+bool conversione (char &s){
+    if(s >= 65 && s <= 90){
+        s = s + 32;
+        return true;
+    }else if(s >= 97 && s <= 122){
+        s = s - 32;
+        return true;
+    }else{
+        return false;
+    }
+}
